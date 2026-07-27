82a62d4db9e4b06f1570b8c8c6066d45abf53ad5
lord fedora <125773867+adekunle-feranmi@users.noreply.github.com>
2026-07-26 23:09:16 -0700

    Fix indentation for SQLALCHEMY_ENGINE_OPTIONS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = cfg.DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
-        app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
         "pool_pre_ping": True,
         "pool_recycle": 280,
     }
